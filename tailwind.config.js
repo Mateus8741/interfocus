a82e3c5f80dfd73cb554d032057e12f50e9db515
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-09-22 12:57:55 -0300

    :sparkles: feat: Config fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        contrast: {
+          300: "#AFDA51",
+          700: "#62A856",
+        },
+
+        text: {
+          100: "#707070",
+          200: "#404040",
+        },
+
+        divider: "#E8E8E8",
+
+        background: "#FAFAFA",
+
+        cardsColor: "#FFFFFF",
+
+        error: "#CE2929",
+      },
+
+      fontFamily: {
+        Regular: 'OpenSans_400Regular',
+        SemiBold: 'OpenSans_600SemiBold',
+        Bold: 'OpenSans_700Bold',
+      },
+    },
   },
   plugins: [],
 }
