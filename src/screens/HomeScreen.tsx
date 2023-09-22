b11a45ab3810057f675e532f212cfe2225b9f172
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-09-22 12:12:38 -0300

    :sparkles: feat: Hook useAppSafeArea

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 
 export function HomeScreen() {
   return (
-    <View>
+    <View className="flex-1 justify-center items-center">
       <Text className="text-red-500">Home</Text>
     </View>
   );
