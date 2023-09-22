d1b41fcd6c5764a9e498c776e917e175482b95a2
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-09-22 12:58:05 -0300

    :sparkles: feat: Config fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,26 @@
-import { Routes } from '@routes';
+import {
+  OpenSans_400Regular,
+  OpenSans_600SemiBold,
+  OpenSans_700Bold,
+  useFonts,
+} from "@expo-google-fonts/open-sans";
+import { Routes } from "@routes";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_600SemiBold,
+    OpenSans_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <Routes />
+    <SafeAreaProvider>
+      <Routes />
+    </SafeAreaProvider>
   );
 }
-
