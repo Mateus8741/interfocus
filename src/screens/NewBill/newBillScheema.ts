f0e412339428910b927a4b5914f562e5580c7383
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-09-24 19:13:19 -0300

    :recycle: refactor: Regex inputs

--- src/screens/NewBill/newBillScheema.ts
+++ src/screens/NewBill/newBillScheema.ts
@@ -2,10 +2,10 @@ import { z } from 'zod';
 
 export const newBillScheema = z.object({
   bill: z.string().min(1, 'Campo obrigatório'),
-  created: z.string().min(1, 'Campo obrigatório').default(''),
-  value: z.string().min(1, 'Campo obrigatório').default(''),
-  status: z.string().min(1, 'Campo obrigatório').default(''),
-  payementDate: z.string().min(1, 'Campo obrigatório').default(''),
+  created: z.string().min(1, 'Campo obrigatório'),
+  value: z.string().min(1, 'Campo obrigatório'),
+  status: z.string().min(1, 'Campo obrigatório'),
+  payementDate: z.string().min(1, 'Campo obrigatório'),
 });
 
 export type NewBillScheema = z.infer<typeof newBillScheema>;
--- src/screens/NewClient/newClientScheema.ts
+++ src/screens/NewClient/newClientScheema.ts
@@ -2,13 +2,9 @@ import { z } from 'zod';
 
 export const newClientScheema = z.object({
   name: z.string().min(1, 'Campo obrigatório'),
-  cpf: z.string().min(1, 'Campo obrigatório').default(''),
-  bday: z.string().min(1, 'Campo obrigatório').default(''),
-  email: z
-    .string()
-    .email('Email inválido')
-    .min(1, 'Campo obrigatório')
-    .default(''),
+  cpf: z.string().min(1, 'Campo obrigatório'),
+  bday: z.string().min(1, 'Campo obrigatório'),
+  email: z.string().email('Email inválido').min(1, 'Campo obrigatório'),
 });
 
 export type NewClientScheema = z.infer<typeof newClientScheema>;
